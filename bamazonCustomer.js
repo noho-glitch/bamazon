7c71d0969ebc88d64bd872d9f386ee618290f87e
Dave Nolan <dpn.nolan@gmail.com>
2019-11-15 15:29:29 -0500

    added display item display and inquirer prompt requesting item number

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var dotenv = require("")
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,6 +12,46 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: process.env.MY_SQL_PASS,
   database: "bamazon_db"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  bamazonStart();
+});
+results = [];
+//query database "bamazon_db" for all items in table "products"
+var bamazonStart = function() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    for(var i = 0; i < results.length; i++) {
+      console.log("\n" +
+        results[i].item_id + " | " + results[i].product_name + " | "  + results[i].department_name + " | " + "$" + results[i].price + " | " + results[i].stock_quantity);
+      }
+  })
+  inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_id);
+            }
+            return choiceArray;
+          },
+          message: "What item number would you like to purchase?"
+        },
+        {
+          name: "quantity",
+          type: "number",
+          message: "How many would you like to buy?"
+        }
+      ]).then(function(checkInventory) {
+        if (checkInventory.quantity <= results[i].stock_quantity)
+          console.log(results[i].price * checkInventory.quantity)
+      })
+}
