b8e81ed785c8d0bbfd104481ab820a86d7ab3e68
Dave Nolan <dpn.nolan@gmail.com>
2019-11-15 17:21:54 -0500

    added console.table and moved quit functionality to be its own prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 require("dotenv").config();
+require("console.table");
 
 //connect to our database
 var connection = mysql.createConnection({
@@ -24,86 +25,98 @@ connection.connect(function(err) {
   displayProducts();
 });
 
-
 results = [];
 
 var displayProducts = function() {
   connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    for(var i = 0; i < results.length; i++) {
-      console.log("\n" +
-        results[i].item_id + " | " + results[i].product_name + " | "  + results[i].department_name + " | " + "$" + results[i].price + " | " + results[i].stock_quantity);
-      }
-      promptUser(results);
-
-  })
-}
+    console.table(results);
+    promptUser(results);
+  });
+};
 
 //query database "bamazon_db" to display all items in table "products"
 var promptUser = function(results) {
-  
-
-
   inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_id);
-            }
-            choiceArray.push("q");
-            return choiceArray;
+    .prompt([
+      {
+        name: "quit",
+        type: "list",
+        message: "Would you like to buy something or quit?",
+        choices: ["BUY SOMETHING", "QUIT"]
+      }
+    ])
+    .then(function(answer) {
+      if (answer.quit === "QUIT") {
+        connection.end();
+        return;
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "rawlist",
+            choices: function() {
+              var choiceArray = [];
+              for (var i = 0; i < results.length; i++) {
+                choiceArray.push(results[i].item_id);
+              }
+              choiceArray.push("q");
+              return choiceArray;
+            },
+            message:
+              "What item number would you like to purchase?"
           },
-          message: "What item number would you like to purchase? ('q' to quit)"       
-        },
-        {
-          name: "quantity",
-          type: "number",
-          message: "How many would you like to buy?"
-        }
-      ]).then(function(answer) {
-        var chosenItem;
-console.log(answer.choice)
+          {
+            name: "quantity",
+            type: "number",
+            message: "How many would you like to buy?"
+          }
+        ])
+        .then(function(answer) {
+          var chosenItem;
+          console.log(answer.choice);
 
-        if (answer.choice === "q") {
-          connection.end()
-          return;
-        }
-
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_id === answer.choice) {
-            chosenItem = results[i]
-           console.log("You chose: " + results[i].product_name);
+          if (answer.choice === "q") {
+            connection.end();
+            return;
           }
 
-        }
-
-        if (answer.quantity <= chosenItem.stock_quantity) {
-          console.log(chosenItem.price * answer.quantity)
-
-          connection.query("UPDATE products SET ? WHERE ?", [
-            {
-              stock_quantity: chosenItem.stock_quantity - answer.quantity
-            },
-            {
-              item_id: chosenItem.item_id
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].item_id === answer.choice) {
+              chosenItem = results[i];
+              console.log("You chose: " + results[i].product_name);
             }
+          }
 
-          ],function(err, results){
-            if (err) throw err;
+          if (answer.quantity <= chosenItem.stock_quantity) {
+            console.log(chosenItem.price * answer.quantity);
 
-            displayProducts();
-          });
-        };
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: chosenItem.stock_quantity - answer.quantity
+                },
+                {
+                  item_id: chosenItem.item_id
+                }
+              ],
+              function(err, results) {
+                if (err) throw err;
 
+                setTimeout(displayProducts, 3000); 
+              }
+            );
+          }
 
+          if (answer.quantity > chosenItem.stock_quantity) {
+            console.log("Sorry that is not available");
 
-        if (answer.quantity > chosenItem.stock_quantity) {
-        console.log("Sorry that is not available")
-          displayProducts();
-        };
-      })
-}
+            setTimeout(displayProducts, 2000); 
+
+          }
+        });
+    });
+};
